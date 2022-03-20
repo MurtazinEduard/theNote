744e61d8bb85684c457408618aa6106ca9d88122
Edward Murtazin <yanaybal.tebya@gmail.com>
2022-03-21 03:32:17 +0500

    adding dist

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/noteList/'
+  publicPath: process.env.NODE_ENV === "production" ? "/noteList/" : "/",
 })
